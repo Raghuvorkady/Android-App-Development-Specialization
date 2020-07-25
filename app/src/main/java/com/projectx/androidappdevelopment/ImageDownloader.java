c32ec6a22c3e6623fc52fd9cd55f19c3c69d49c6
Raghuvorkady <51401827+Raghuvorkady@users.noreply.github.com>
2020-07-25 15:52:08 +0530

    added save to device option to the users, added SplashScreen

--- app/src/main/java/com/projectx/androidappdevelopment/ImageDownloader.java
+++ app/src/main/java/com/projectx/androidappdevelopment/ImageDownloader.java
@@ -1,6 +1,11 @@
 package com.projectx.androidappdevelopment;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -12,26 +17,43 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.ContentValues.TAG;
+
 //TODO save the file downloaded into the device!!!
 public class ImageDownloader extends Fragment {
     String url = "https://wallpapersite.com/images/pages/pic_w/6408.jpg";
     ImageView image;
-    Button button;
+    Button downloadButton;
     EditText editText;
+    RelativeLayout relativeLayout;
     ProgressDialog mProgressDialog;
+
     public ImageDownloader() {
         // Required empty public constructor
     }
@@ -53,54 +75,97 @@ public class ImageDownloader extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         image = (ImageView) getView().findViewById(R.id.imageToDl);
-        button = (Button) getView().findViewById(R.id.downloadB);
+        downloadButton = (Button) getView().findViewById(R.id.downloadB);
         editText = (EditText) getView().findViewById(R.id.searchBox);
+        relativeLayout = (RelativeLayout) getView().findViewById(R.id.relativeLayoutID);
 
+        image.setDrawingCacheEnabled(true);
         String stringTab1 = "ImageDownloader in Android";
         getActivity().setTitle(stringTab1 + "");
-        getActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.darkGreen));
-        getActivity().setTitleColor(getResources().getColor(R.color.colorPrimaryDark));
+        getActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
 
-        button.setOnClickListener(new View.OnClickListener() {
+        downloadButton.setVisibility(View.INVISIBLE);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    try {
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(relativeLayout.getWindowToken(), 0);
+                    } catch (Exception e) {
+                    }
+                    url = editText.getText().toString();
+                    new DownloadImage().execute(url);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url = editText.getText().toString();
-                new DownloadImage().execute(url);
+                storeImage(image.getDrawingCache());
             }
         });
     }
 
-    private class DownloadImage extends AsyncTask<String,Void,Bitmap> {
+    private void storeImage(Bitmap bitmap) {
+        File checkFile = new File(String.valueOf(getActivity().getApplicationContext().getExternalFilesDir("/" + "AAD Basics" + "/" + "ImageDownloader")));
+        String timeStamp = new SimpleDateFormat("YYYYMMDD_HHmm").format(new Date());
+        String imageName =  timeStamp + ".jpg";
+        File file = new File(checkFile, imageName);
+        if (!file.exists()) {
+            Log.d("path", file.toString());
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                Toast.makeText(getActivity().getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+                fos.flush();
+                fos.close();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             mProgressDialog = new ProgressDialog(getActivity());
-            mProgressDialog.setTitle("Download Image Tutorial");
+            mProgressDialog.setTitle("Downloading the Image");
             mProgressDialog.setMessage("Loading...");
             mProgressDialog.setIndeterminate(false);
             mProgressDialog.show();
         }
+
         @Override
         protected Bitmap doInBackground(String... URL) {
             String imageURL = URL[0];
             HttpURLConnection connection = null;
-            try{
+            try {
                 connection = (HttpURLConnection) new URL(imageURL).openConnection();
                 connection.connect();
                 InputStream inputStream = connection.getInputStream();
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
                 return BitmapFactory.decodeStream(bufferedInputStream);
-            }catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
         }
+
         // When all async task done
-        protected void onPostExecute(Bitmap result){
+        protected void onPostExecute(Bitmap result) {
             // Hide the progress dialog
             mProgressDialog.dismiss();
-            if(result!=null){
+            if (result != null) {
                 image.setImageBitmap(result);
+                downloadButton.setVisibility(View.VISIBLE);
             } else {
                 // Notify user that an error occurred while downloading image
                 Toast.makeText(getActivity().getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
